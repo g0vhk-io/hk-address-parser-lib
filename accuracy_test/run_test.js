e363848f766a8e2a7e86589c7d1a244d70d83455
Nandi Wong <nandi0315@gmail.com>
2019-03-15 13:26:16 +0800

    tweak the performance of the accuracy_test
    use module instead of spawn to speed up the test by ~10x
    coz no more python tests now

--- accuracy_test/run_test.js
+++ accuracy_test/run_test.js
@@ -1,81 +1,21 @@
-const md5 = require('md5');
-
 const addressResolver = require('./../dist/hk-address-parser.cjs.js');
-const Promise = require('bluebird');
-const fs = require('fs');
-const path = require('path');
-
-if (process.argv.length < 2) {
-  console.log('{}');
-}
 
 
-const address = process.argv[2];
-
-/*
- * =========   Trying add a layer of cache to speed up local test ===========
- */
-function cacheFilePathForUrl(url) {
-  // Base64 would return '/' but we do not want for paths
-  return path.join('.cache', md5(url));
-}
-
-function saveToCache(url, json) {
-  if (!fs.existsSync('.cache')){
-    fs.mkdirSync('.cache');
-  }
-  const filePath = cacheFilePathForUrl(url);
-  fs.writeFileSync(filePath, JSON.stringify(json));
-}
-
-function loadFromCache(url) {
-  const filePath = cacheFilePathForUrl(url);
-  if (!fs.existsSync(filePath)) {
-    return null;
-  }
-  return fs.readFileSync(filePath).toString();
-}
-
-// Replace the fetch function
-const nodeFetch = global.fetch;
-global.fetch = function (...args) {
-  const url = args[0];
-
-  // try to load the cached files
-  const cache = loadFromCache(url);
-  if (cache) {
-    return new Promise((resolve) => {
-      resolve({
-        json: () => JSON.parse(cache)
-      });
-    });
-  } else {
-    // Hook the function
-    const fn = new Promise((resolve, reject) => {
-      nodeFetch(...args).then(res => {
-        // Overwrite the json()
-        res.json().then(json => {
-          saveToCache(url, json);
-          res.json = () => json;
-          resolve(res);
-        })
-
-      })
-    });
-    return fn;
+module.exports = {
+  run: async (address) => {
+    try {
+      const results = await addressResolver.parse(address);
+      const result = results[0];
+      const obj = {
+        eng: result.fullAddress('eng'),
+        chi: result.fullAddress('chi'),
+        geo: result.coordinates().map(coord => ({Latitude: coord.lat + '', Longitude: coord.lng + ''}))
+      };
+      return obj;
+    } catch (error) {
+      console.error(error);
+      return {};
+    }
   }
 }
 
-
-addressResolver.parse(address).then(results => {
-  const result = results[0];
-  const obj = {
-    eng: result.fullAddress('eng'),
-    chi: result.fullAddress('chi'),
-    geo: result.coordinates().map(coord => ({Latitude: coord.lat + '', Longitude: coord.lng + ''}))
-  };
-  console.log(JSON.stringify(obj));
-}).catch(error => {
-   console.log(error.stack);
-  console.log('{}');
-})
