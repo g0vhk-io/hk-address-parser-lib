9cb708de09d333a9e8a929be647a3ca81eeb878e
Nandi Wong <nandi0315@gmail.com>
2019-04-03 14:50:29 +0800

    Fix a bug on the LandAddress

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # https://blog.travis-ci.com/2018-04-11-how_to_publish_node_js_packages_with_travis_ci_and_packagecloud/
 echo "//registry.npmjs.org/:_authToken=${NPM_API_TOKEN}" > $HOME/.npmrc
 
-# npm publish
+npm publish
 
 # patch using npm?
 # npm version patch
