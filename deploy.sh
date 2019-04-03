373d5bd4d0c01c26346f6b77ac85de1c4ceb8b8c
Nandi Wong <nandi0315@gmail.com>
2019-04-03 15:03:36 +0800

    add also npmjs.com for deploying

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 # inspired by 
 # https://blog.travis-ci.com/2018-04-11-how_to_publish_node_js_packages_with_travis_ci_and_packagecloud/
 echo "//registry.npmjs.org/:_authToken=${NPM_API_TOKEN}" > $HOME/.npmrc
+echo "//registry.npmjs.com/:_authToken=${NPM_API_TOKEN}" > $HOME/.npmrc
 
 npm publish
 
