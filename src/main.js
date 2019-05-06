43f6cbae9a8964ed532a8efe82e3a8a2555f6294
Nandiheath <nandi0315@gmail.com>
2019-05-06 22:23:24 +0800

    add batch process

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ export const parse = (address) => {
 	return resolver.queryAddress(address);
 }
 
+export const parseBatch = (addresses, options = {
+  limit: 10,
+}) => {
+  return resolver.queryMultipleAddress(addresses, options);
+}
+
 // named export for Address model
 export const Address = address;
 
